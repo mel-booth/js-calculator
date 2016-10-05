7cbebe1daee7e918459615bfa76957935d7ac16a
Mel Booth <mel.m.booth@gmail.com>
2016-10-06 11:56:28 +1300

    add variables to script file

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  //Stores user input to calculate later on
+  var inputs = [" "];
+
+  //String to store current input
+  var totalString;
+
+  //Operators array - exluding .
+  var operators1 = ["+", "-", "/", "*"];
+
+  //Operators array including .
+  var operators2 = ["."];
+
+  //Numbers array for validation
+  var nums = [0,1,2,3,4,5,6,7,8,9];
+
+});
