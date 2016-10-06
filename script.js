336778b340d4ff1073f8bd5da84ce46449c5b19b
Mel Booth <mel.m.booth@gmail.com>
2016-10-06 14:43:25 +1300

    add some comments to script to understand what's happening

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  //Stores user input to calculate later on
+  //Stores user input to be calculated
   var inputs = [" "];
 
   //String to store current input
@@ -9,22 +9,27 @@ $(document).ready(function(){
   //Operators array - exluding .
   var operators1 = ["+", "-", "/", "*"];
 
-  //Operators array including .
+  //Operators array, only .
   var operators2 = ["."];
 
-  //Numbers array for validation
+  //Numbers array
   var nums = [0,1,2,3,4,5,6,7,8,9];
 
+  //Adds value to the inputs array
   function getValue(input){
+    //Displays error message if you try and use two decimals (.) in a row
     if(operators2.includes(inputs[inputs.length-1]===true && input===".")){
       console.log("Duplicate '.'");
     }
+    //To make sure you can't input an operator from operators1 array before a number
     else if(inputs.length===1 && operators1.includes(input)===false){
       inputs.push(input);
     }
+    //Adds an operator to the output only if last character input was not an operator
     else if(operators1.includes(inputs[inputs.length-1])===false){
       inputs.push(input);
     }
+    //So you can add multiple numbers to the output
     else if(nums.includes(Number(input))){
       inputs.push(input);
     }
@@ -45,11 +50,12 @@ $(document).ready(function(){
   }
 
   $("button").on("click", function(){
-
+    // If CLEAR button is clicked, updates value in inputs array to its default (nothing)
     if (this.id==="clear"){
       inputs=[""];
       updateValue();
     }
+    // If ='s button is clicked, activates getTotal function
     else if (this.id==="total"){
       getTotal();
     }
