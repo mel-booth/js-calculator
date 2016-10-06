79cadd045daaf20f331a76f0406b999ffef0a83c
Mel Booth <mel.m.booth@gmail.com>
2016-10-06 14:28:53 +1300

    update getValue function

--- script.js
+++ script.js
@@ -16,14 +16,25 @@ $(document).ready(function(){
   var nums = [0,1,2,3,4,5,6,7,8,9];
 
   function getValue(input){
-
+    if(operators2.includes(inputs[inputs.length-1]===true && input===".")){
+      console.log("Duplicate '.'");
+    }
+    else if(inputs.length===1 && operators1.includes(input)===false){
+      inputs.push(input);
+    }
+    else if(operators1.includes(inputs[inputs.length-1])===false){
+      inputs.push(input);
+    }
+    else if(nums.includes(Number(input))){
+      inputs.push(input);
+    }
+    updateValue();
 
   }
 
   function updateValue(){
     totalString = inputs.join("");
     $("#output").html(totalString);
-
 
   }
 
@@ -35,7 +46,7 @@ $(document).ready(function(){
 
   $("button").on("click", function(){
 
-    if(this.id==="clear"){
+    if (this.id==="clear"){
       inputs=[""];
       updateValue();
     }
@@ -43,7 +54,7 @@ $(document).ready(function(){
       getTotal();
     }
     else {
-      if(inputs[inputs.length-1].indexOf("+", "-", "/", "*", "-1")===-1){
+      if(inputs[inputs.length-1].indexOf("+", "-", "/", "*", ".")===-1){
         getValue(this.id);
       }
       else{
